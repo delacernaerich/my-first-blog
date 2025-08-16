a963c177d990b747190780ea9389cb93b3dc1e8f
erich <delacernaerichlorain@gmail.com>
2025-08-16 15:36:03 +0800

    Fix errors

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_create, name='post_create'),
 ]
